e2b2a9141593d19ff220f269c26ef3bbf874ceb4
kbub27 <kalebmerriman27@gmail.com>
2019-07-10 17:52:18 -0700

    Updates

---
+++ precache-manifest.e9acd5e2a6890135d8e9f1ef4d268afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05827362edb2cbc499fa02a2ca285c6",
+    "url": "/ClickClick/index.html"
+  },
+  {
+    "revision": "5b15946fcafae1606003",
+    "url": "/ClickClick/static/css/main.bdb76840.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickClick/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5b15946fcafae1606003",
+    "url": "/ClickClick/static/js/main.748dbf0d.chunk.js"
+  },
+  {
+    "revision": "4d7d232af97864a80b46",
+    "url": "/ClickClick/static/js/runtime~main.c9b2afb7.js"
+  }
+]);
